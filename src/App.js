a3b105c9dea0bf7f3e608099e63ab1020d23e6ef
goranaviani <goranaviani@gmail.com>
2022-06-10 21:02:39 +0200

    w3resource_js_functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Functions from "./components/Functions";
 
 function App() {
   return (
-    <>App</>
+    <>
+      App
+      <Functions/>
+
+    </>
   );
 }
 
