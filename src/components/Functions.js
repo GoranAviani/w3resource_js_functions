d12416bb17b874b5f80e452e723f795515a5090a
goranaviani <goranaviani@gmail.com>
2022-06-10 21:06:57 +0200

    w3resource_js_functions

--- src/components/Functions.js
+++ src/components/Functions.js
@@ -3,11 +3,22 @@ import React from 'react';
 
 const Functions = () => {
 
+/*
+1. Write a JavaScript function that reverse a number. Go to the editor
+Example x = 32243;
+Expected Output : 34223
 
+ */
+
+    const first = (number=  1234) => {
+        console.log(number)
+
+    }
 
     return(
         <>
         functions
+            {first()}
         </>
     )
 }
